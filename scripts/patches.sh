670f8c8e777ec6332088095652431f10f5e9660b
John Doe <klever1988@protonmail.com>
2024-10-20 12:31:50 +0800

    fix

--- scripts/patches.sh
+++ scripts/patches.sh
@@ -124,7 +124,7 @@ if [[ $DEVICE == 'r1s' ]]; then
 
   sed -i '/luci/d' $GITHUB_WORKSPACE/common.seed $GITHUB_WORKSPACE/extra_packages.seed
 
-  git revert --no-edit f4405a9597eea92622b66f122de2fb738a605d5d 51459ab19ec99ac63090766dff5dbc8ae74ef714 
+  git revert --no-commit f4405a9597eea92622b66f122de2fb738a605d5d 51459ab19ec99ac63090766dff5dbc8ae74ef714 
 fi
 
 # fix for r1s-h3
